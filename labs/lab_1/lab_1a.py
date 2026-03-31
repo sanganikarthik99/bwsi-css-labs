eba5b08c924fadac91b532ddf33bda49ea6c4d0a
Karthik Sangani <sanganikarthik99@gmail.com>
2026-03-30 21:21:32 -0400

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Karthik" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello everyone! My name is Karthik Sangani, and I am currently a sophmore in high school from Northern Virginia. I currently attend Thomas Jefferson High School for Science and Technology, and some of my hobbies/interests include reading, basketball, and photography. I want to learn as much as I can about artificial intelligence and machine learning so I can use them to build something that can help people. I'm not quite sure which direction specifically I want this go in, but I want to learn as much as I can before I start to plan and build.")
 
 if __name__ == "__main__":
     main()
